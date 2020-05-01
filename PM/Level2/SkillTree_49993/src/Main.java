1c3612036d1576b56f349a1fa58d3d97cbb24a7d
JeonHyungJin <qpdjgjsl@gmail.com>
2020-05-02 01:13:04 +0900

    SkillTree

---
+++ PM/Level2/SkillTree_49993/src/Main.java
@@ -0,0 +1,45 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String skill = "CBD";
+		String[] skill_trees = { "BACDE", "CBADF", "AECB", "BDA" };
+
+		System.out.println(solution(skill, skill_trees));
+
+	}
+
+	public static int solution(String skill, String[] skill_trees) {
+		int answer = 0;
+
+		for (int i = 0; i < skill_trees.length; i++) {
+			boolean flag = true;
+			String nowTree = skill_trees[i];
+			String front ="", back = "";
+			for (int j = 0; j < skill.length(); j++) {
+				char sk = skill.charAt(j);
+				int index = nowTree.indexOf(sk);
+				
+				if(front.contains(String.valueOf(sk))) {
+					flag = false;
+					break;
+				}
+				if(index == -1) {
+					front = nowTree;
+				} else {
+					
+					front = nowTree.substring(0, index);
+					back = nowTree.substring(index + 1);
+				}
+			}
+			if(flag) {
+				answer++;
+			}
+		}
+
+		return answer;
+	}
+
+}
