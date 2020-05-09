edcaa04ad4a54f069cd5fd20cc54f86f79313221
JeonHyungJin <qpdjgjsl@gmail.com>
2020-05-09 13:43:15 +0900

    2020Kakao Summer Intern init.

---
+++ 2020_SummerKakaoIntern/Prob1/src/Main.java
@@ -0,0 +1,10 @@
+//Kakao Summer Intern Prob1
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ 2020_SummerKakaoIntern/Prob4/src/Main.java
@@ -0,0 +1,10 @@
+//Kakao Summer Intern Prob4
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
